1e24254b61785b826a91c7641659a430e7afe593
deepanshu malviya <mdeepanshu143@gmail.com>
2022-06-20 02:17:06 +0530

    update and delete project

--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -1,12 +1,11 @@
 import { useQuery } from "@apollo/client";
 import ClientRow from "./ClientRow";
 import { GET_CLIENTS } from "../queries/clientQueries";
-import Spinner from "./Spinner";
 
 export default function Clients() {
   const { loading, error, data } = useQuery(GET_CLIENTS);
 
-  if (loading) return <Spinner />;
+  if (loading) return <p>Loading...</p>;
   if (error) return <p>Something Went Wrong</p>;
 
   return (
--- client/src/components/Spinner.jsx
+++ client/src/components/Spinner.jsx
@@ -1,8 +1,7 @@
 export default function Spinner() {
   return (
     <div className="d-flex justify-content-center">
-      <div className="spinner-border" role="status">
-      </div>
+      <div className="spinner-border" role="status"></div>
     </div>
   );
 }
